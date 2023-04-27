0558ec66d35aabf032a33b270a3f1309b4073e2f
iuvarkin <uvarkin77@gamil.com>
2023-04-28 00:10:13 +0300

    add test for sum complex number

---
+++ modules/complex-number/test/test_UVARKIN_ILYA_complex_number.cpp
@@ -0,0 +1,36 @@
+// Copyright 2023 ILYA UVARKIN
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Uvarkin_Ilya_ComplexNumberTest, SumImaginaryParts) {
+  ComplexNumber num1(0, 40);
+  ComplexNumber num2(0, 2);
+
+  ComplexNumber rez = num1 + num2;
+
+  ASSERT_EQ(rez.getRe(), 0);
+  ASSERT_EQ(rez.getIm(), 42);
+}
+
+TEST(Uvarkin_Ilya_ComplexNumberTest, SumRealParts) {
+  ComplexNumber num1(20, 0);
+  ComplexNumber num2(5, 0);
+
+  ComplexNumber rez = num1 + num2;
+
+  ASSERT_EQ(rez.getRe(), 25);
+  ASSERT_EQ(rez.getIm(), 0);
+}
+
+TEST(Uvarkin_Ilya_ComplexNumberTest, SumComplexNumber) {
+  ComplexNumber num1(20, -5);
+  ComplexNumber num2(-5, 10);
+
+  ComplexNumber rez = num1 + num2;
+
+  ASSERT_EQ(rez.getRe(), 15);
+  ASSERT_EQ(rez.getIm(), 5);
+}
+
