baa09ec905440160158f66488cfd937f731ea148
iuvarkin <uvarkin77@gamil.com>
2023-05-13 11:24:23 +0300

    add union, intersection, difference

--- modules/sets/include/set.h
+++ modules/sets/include/set.h
@@ -10,8 +10,14 @@ public:
     void erase(int elem);
     int count(int elem);
 
+    const std::vector<int> &getElems() const;
+
     Set()= default;
     explicit Set(const std::vector<int> &elems) : elems(elems) {}
+
+    friend Set operator & (const Set& set1, const Set& set2);
+    friend Set operator | (const Set& set1, const Set& set2);
+    friend Set operator - (const Set& set1, const Set& set2);
 private:
     std::vector<int> elems;
 };
--- modules/sets/test/text_set.cpp
+++ modules/sets/test/text_set.cpp
@@ -1,6 +1,7 @@
 // Copyright 2023 Uvarkin Ilya
 
 #include <gtest/gtest.h>
+#include <cstring>
 #include "include/set.h"
 
 TEST(Set, assert_no_throw_in_default_constructor) {
@@ -37,3 +38,44 @@ TEST(Set, erase_elem) {
 
   EXPECT_EQ(set.count(42), 0);
 }
+
+TEST(Set, set_union) {
+  Set set1(std::vector<int>{1, 3, 7});
+  Set set2(std::vector<int>{1, 2, 3, 15});
+  std::vector<int> ElemsExpect = std::vector<int>{1, 2, 3, 7, 15};
+
+  Set set = set1 | set2;
+
+  ASSERT_EQ(set.getElems().size(), ElemsExpect.size());
+  for (long unsigned int i = 0; i < set.getElems().size(); ++i) {
+      EXPECT_EQ(set.getElems()[i], ElemsExpect[i]);
+  }
+}
+
+TEST(Set, set_intersection) {
+  Set set1(std::vector<int>{1, 2, 3, 4, 7});
+  Set set2(std::vector<int>{1, 3, 15});
+  std::vector<int> ElemsExpect = std::vector<int>{1, 3};
+
+  Set set = set1 & set2;
+
+  ASSERT_EQ(set.getElems().size(), ElemsExpect.size());
+  for (long unsigned int i = 0; i < set.getElems().size(); ++i) {
+    EXPECT_EQ(set.getElems()[i], ElemsExpect[i]);
+  }
+}
+
+TEST(Set, set_difference) {
+  Set set1(std::vector<int>{1, 2, 3, 4, 7});
+  Set set2(std::vector<int>{1, 3, 15});
+  std::vector<int> ElemsExpect = std::vector<int>{2, 4, 7};
+
+  Set set = set1 - set2;
+
+  ASSERT_EQ(set.getElems().size(), ElemsExpect.size());
+  for (long unsigned int i = 0; i < set.getElems().size(); ++i) {
+    EXPECT_EQ(set.getElems()[i], ElemsExpect[i]);
+  }
+}
+
+
